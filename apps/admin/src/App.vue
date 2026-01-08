6041709ae0c9947a6b736241cc86c7d6088c9e46
m0dh4x <info@florian-drews.de>
2026-01-08 22:37:19 +0100

    feat: add admin application
    -create minimal admin interface

---
+++ apps/admin/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useSession } from "@vela/auth/client";
+
+const { data: session } = useSession();
+</script>
+
+<template>
+  <div>
+    <h1>Vela CMS Admin</h1>
+    <p v-if="session">Welcome, {{ session.user.email }}</p>
+    <p v-else>Please sign in</p>
+  </div>
+</template>
