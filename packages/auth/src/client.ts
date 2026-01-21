bf11b2d72b40323e18cfdcc8678cff4e1bb1bee7
m0dh4x <info@florian-drews.de>
2026-01-21 13:25:44 +0100

    chore(auth): add baseurl to allow cross origin request -between frontend and api

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,5 +1,10 @@
 import { createAuthClient } from 'better-auth/vue';
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+	baseURL: 'http://localhost:3000',
+});
 
-export const { signIn, signUp, signOut, useSession } = authClient;
+export const { signIn, signOut, signUp, useSession } = authClient;
+
+export type Session = typeof authClient.$Infer.Session;
+export type User = typeof authClient.$Infer.Session.user;
